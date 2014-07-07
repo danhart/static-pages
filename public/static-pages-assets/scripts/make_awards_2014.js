7f8b654e64cf5c44451e7262237ce654591c2c6a
Gilberto Lenzi <gilbertolenzi@notonthehighstreet.com>
2014-07-07 09:40:23 +0100

    Add the make awards 2014 layouts

--- public/static-pages-assets/scripts/make_awards_2014.js
+++ public/static-pages-assets/scripts/make_awards_2014.js
@@ -1,3 +1,26 @@
 define(function() {
-    // console.log("test");
+
+    $(window).scroll(function(e){
+      parallax();
+    });
+    function parallax(){
+      var scrolled = $(window).scrollTop();
+      $('.background').css('top',-(scrolled*0.4)+'px');
+    }
+
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 700);
+            return false;
+          }
+        }
+      });
+    });
+
 });
