dd5d5ed45d479ef9d4ac05bece52d5fb29884d91
Dan Hart <danhart@notonthehighstreet.com>
2014-08-13 14:23:27 +0100

    Passenger compatibility changes

--- app.js
+++ app.js
@@ -1,76 +1,13 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var lessMiddleware = require('less-middleware');
-var bodyParser = require('body-parser');
-var layoutFetcher = require('layout-fetcher');
+#!/usr/bin/env node
+var debug = require('debug')('static-pages');
+var app = require('./express');
 
-var appConfig = require('./app-config');
-var routes = require('./routes/index');
+var http = require('http');
+var https = require('https');
 
-var layoutService = require('./services/layout-service');
+http.globalAgent.maxSockets = 500;
+https.globalAgent.maxSockets = 500;
 
-var app = express();
+app.set('port', process.env.PORT || 3000);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(favicon());
-
-app.use(logger(appConfig.loggerMode));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(lessMiddleware(path.join(__dirname, 'public')));
-
-app.use(express.static(path.join(__dirname, appConfig.publicDir)));
-
-app.use(layoutFetcher({
-    url:         appConfig.nothsLayoutUrl,
-    cacheLayout: appConfig.cacheLayout,
-    done:        function(layout) {
-        layoutService.layout = layout;
-    }
-}));
-
-app.use('/', routes);
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-module.exports = app;
+app.listen(app.get('port'));
